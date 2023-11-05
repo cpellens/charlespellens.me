0ee9cc1ccb7ac1439bd3cb2f719f2521f2102475
Charles Pellens <contact@charlespellens.com>
2023-11-04 23:51:56 -0400

    Fix dayjs dilemma

--- src/lib/components/Footer/Bar.svelte
+++ src/lib/components/Footer/Bar.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
     import { env } from "$env/dynamic/public";
-    import dayjs from "dayjs/esm"
 </script>
 
 <footer class="container mx-auto mt-4 grid grid-cols-2">
     <aside>
-        Created by Charles &bull; Copyright &copy; 2021&ndash;{dayjs().year().toString()} - All rights reserved.
+        Created by Charles &bull; Copyright &copy; 2021&ndash;{(new Date()).getFullYear()} - All rights reserved.
     </aside>
 
     {#if env.PUBLIC_EMAIL_CONTACT_ME}
